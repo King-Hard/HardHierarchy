fa4694761b662466dc5dda0a1de4ad685bdb4d3a
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-05-30 18:25:23 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const increment = () => {
+    setCount(count + 1)
+  }
+
   return (
-    <div className="text-2xl text-red-500 bg-red-100">
-      Hello World
-    </div>
+    <>
+
+      <div className='bg-gray-200 flex items-center justify-center min-h-screen'>
+        <div className='border p-7 rounded-xl bg-white shadow-lg w-100'>
+          <button onClick={increment} className='border rounded p-2 bg-green-300'>Increment</button>
+
+          <div className='border rounded bg-red-300 p-2 w-25 float-end'>Count: {count}</div>
+        </div>
+      </div>
+
+    </>
   )
 }
 
---
+++ src/Readme.md
@@ -0,0 +1,4 @@
+# Events 
+
+1, onChnage
+2, onClick
