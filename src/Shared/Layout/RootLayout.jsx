0a4f82f435b5b00cdcf952cde0c160e501c21e62
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-06-01 14:04:00 +0800

    .

--- src/Shared/Layout/RootLayout.jsx
+++ src/Shared/Layout/RootLayout.jsx
@@ -1,5 +1,6 @@
 import Header from '../Partial/Header'
 import Footer from '../Partial/Footer'
+import { Link } from 'react-router-dom'
 
 const RootLayout = ({children}) => {
 
@@ -8,6 +9,12 @@ const RootLayout = ({children}) => {
 
       <div class="root-main" className='bg-gray-200 flex items-center justify-center min-h-screen'>
         <div className='border p-7 rounded-xl bg-white shadow-lg w-100'>
+          <div className='space-x-10 p-2'>
+            <Link to="/Home">Home Page</Link>
+            <Link to="/About">About Page</Link>
+            <Link to="/Cars">Cars Page</Link>
+          </div>
+
           <Header />
 
             {children}
