4f9e9908c8812ebb9dc935bef976d4717927bd42
King-Hard <208336213+King-Hard@users.noreply.github.com>
2025-06-01 16:26:19 +0800

    .

--- FolderStructure/Shared/Partial/Head.jsx
+++ FolderStructure/Shared/Partial/Head.jsx
@@ -1,7 +1,16 @@
+import  { Link } from 'react-router-dom'
+
 const Header = () => {
     <>
-        <div>
-            Head
+        <div className='border'>
+            <div>
+                <div>Header</div>
+                <div>
+                    <Link to="/Home">Home Page</Link>
+                    <Link to="/About">About Page</Link>
+                    <Link to="/Contact">Contanct Page</Link>
+                </div>
+            </div>
         </div>
     </>
 }
